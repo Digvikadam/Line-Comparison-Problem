984b0c7cb8a077418a772177623822fc7f7955f9
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-17 00:35:32 +0530

    Added: Equality-of-Two-Lines

--- LineComparision.java
+++ LineComparision.java
@@ -3,17 +3,16 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class LineComparision {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Co-ordinates:");
-		int x1 = sc.nextInt();
-		int y1 = sc.nextInt();
-		System.out.println("Enter Second Co-ordinates:");
-		int x2 = sc.nextInt();
-		int y2 = sc.nextInt();
-		double length;
-		
-		length = Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1),2));
-		System.out.println("The length of line is:" +length);	
+		System.out.println("Welcome to the Line Comparision Computation program");
+		double x1=5,x2=6,y1=4,y2=5;
+		double x3=6,x4=7,y3=5,y4=7;
+		double LENGTH_OF_LINE1,LENGTH_OF_LINE2;
+		LENGTH_OF_LINE1 = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+		LENGTH_OF_LINE2 = Math.sqrt(Math.pow((x4-x3),2) + Math.pow((y4-y3),2));
+		System.out.println("length of the line 1 : " +LENGTH_OF_LINE1);
+		System.out.println("length of the line 2 : " +LENGTH_OF_LINE2);
+		boolean EQUAL = (LENGTH_OF_LINE1 == LENGTH_OF_LINE2);
+		System.out.println("Is It Equal : "+EQUAL);
 	}
 
 }
