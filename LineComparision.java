43fcada15079a46dfd0cc5a9b5c75a4af56ba2fc
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-17 00:41:05 +0530

    Added: Checking-Two-Lines

--- LineComparision.java
+++ LineComparision.java
@@ -3,7 +3,6 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class LineComparision {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Line Comparision Computation program");
 		double x1=5,x2=6,y1=4,y2=5;
 		double x3=6,x4=7,y3=5,y4=7;
 		double LENGTH_OF_LINE1,LENGTH_OF_LINE2;
@@ -13,6 +12,12 @@ public class LineComparision {
 		System.out.println("length of the line 2 : " +LENGTH_OF_LINE2);
 		boolean EQUAL = (LENGTH_OF_LINE1 == LENGTH_OF_LINE2);
 		System.out.println("Is It Equal : "+EQUAL);
+		int Line = Double.compare(LENGTH_OF_LINE1,LENGTH_OF_LINE2); 
+		if (Line == 1){
+			System.out.println("line1 is greater than line 2");
+		}
+		else 
+			System.out.println("line1 is lesser than line 2");
 	}
 
 }
